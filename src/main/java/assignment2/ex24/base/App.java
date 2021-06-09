0dea76e56287ad1b1e807190911f4e7b94bd20c8
Justin Laptop <cepedaj0601@gmail.com>
2021-06-08 22:30:37 -0400

    Initial commit

---
+++ src/main/java/assignment2/ex24/base/App.java
@@ -0,0 +1,35 @@
+package assignment2.ex24.base;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 ex24 Solution
+ *  Copyright 2021 Justin Cepeda
+ */
+
+/*
+Exercise 24 - Anagram Checker
+Using functions to abstract the logic away from the rest of your code makes it easier to read and easier to maintain.
+
+Create a program that compares two strings and determines if the two strings are anagrams. The program should prompt for
+both input strings and display the output as shown in the example that follows.
+
+Example Output
+Enter two strings and I'll tell you if they are anagrams:
+Enter the first string: note
+Enter the second string: tone
+"note" and "tone" are anagrams.
+Constraints
+Implement the program using a function called isAnagram, which takes in two words as its arguments and returns true or
+false. Invoke this function from your main program.
+Check that both words are the same length.
+Challenge
+Complete this program without using built-in language features. Use selection or repetition logic instead and develop
+your own algorithm.
+ */
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    }
+}
