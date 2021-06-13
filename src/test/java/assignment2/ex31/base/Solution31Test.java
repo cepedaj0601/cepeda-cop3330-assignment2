bd7c80528759bc988da943fc9b0a1103a79b8da6
Justin Laptop <cepedaj0601@gmail.com>
2021-06-13 14:02:32 -0400

    Solution 31 commit

---
+++ src/test/java/assignment2/ex31/base/Solution31Test.java
@@ -0,0 +1,27 @@
+package assignment2.ex31.base;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution31Test {
+
+  /*
+    @Test
+    void DefaultTarget(){
+        for (double i = 55; i <96 ; i++) {
+            String actual = Solution31.targetHeartRate(22, 65, i/100);
+            String expected = (int)(i) + "%          | " + (int)TargetHeartRate + " bpm";;
+        }
+
+
+    }
+    @Test
+    void DefaultTable(){
+        Solution31.tableMaker();
+    }*/
+
+    //output of the functions are repeated lines containing ints within the strings. I couldn't figure out how to test
+    //for only the ints within the strings and not the whole thing :(
+}
+
