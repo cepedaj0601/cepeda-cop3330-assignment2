73bc35be8ce1c6af5ba90bab1cfcf3f56572cfb3
Justin Laptop <cepedaj0601@gmail.com>
2021-06-14 00:05:19 -0400

    ahhh midnight

--- src/test/java/assignment2/ex32/base/GameTest.java
+++ src/test/java/assignment2/ex32/base/GameTest.java
@@ -1,14 +1,50 @@
 package assignment2.ex32.base;
 
+import assignment2.ex37.base.Solution37;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
+/*
+//In order to test the method i would needa return variable, however given that the code runs on a while loop and the
+game can go on infinitely, I could not figure out how to incorporate these tests
+    @Test
+    void Difficulty1NumberTest() {
+        String actual = Game.guessingGame("5", 0, "Y", 5, 1);
+        String expected = "You got it in 1 guesses!";
+    }
 
     @Test
-    void guessingGame() {
-        String actual
+    void Difficulty1LetterTest() {
+        String actual = Game.guessingGame("a", 0, "Y", 5, 1);
+        String expected = "That's not a number. Guess again:";
     }
+
+    @Test
+    void Difficulty2NumberTest() {
+        String actual = Game.guessingGame("55", 0, "Y", 55, 2);
+        String expected = "You got it in 1 guesses!";
+    }
+
+    @Test
+    void Difficulty2LetterTest() {
+        String actual = Game.guessingGame("a", 0, "Y", 55, 2);
+        String expected = "That's not a number. Guess again:";
+    }
+
+    @Test
+    void Difficulty3NumberTest() {
+        String actual = Game.guessingGame("555", 0, "Y", 555, 3);
+        String expected = "You got it in 1 guesses!";
+    }
+
+    @Test
+    void Difficulty3LetterTest() {
+        String actual = Game.guessingGame("a", 0, "Y", 555, 3);
+        String expected = "That's not a number. Guess again:";
+    }
+
+ */
 }
